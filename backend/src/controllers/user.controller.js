c0bd76c407e49bc2280c767459e170e6a913f857
Your Name <you@example.com>
2025-12-06 20:42:26 +1000

    added user logout service

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -66,7 +66,33 @@ const loginUser = async(req, res) => {
         })
     }
 }
+
+const logoutUser = async(req, res) => {
+    try {
+        const {email} = req.body;
+
+        const user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Logout successful"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "internal server error", error
+        });
+    }
+}
 export {
+    logoutUser,
     registerUser,
     loginUser
 };
