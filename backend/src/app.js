89707819818e1654779335e751aab45f563f5f5c
Your Name <you@example.com>
2025-12-06 19:29:28 +1000

    added router

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+ 
+import userRouter from "./routes/user.route.js";
+
+app.use("/api/v1/users", userRouter);
+
+
+
 export default app;
